cbdf7511bc65f16a2c66962c556befa2bc06d9ed
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-09-07 16:43:04 -0400

    Assignment 3 in progress

--- assignment3.py
+++ assignment3.py
@@ -1,6 +1,20 @@
 import argparse
+import requests
+import csv
+import logging
+import re
 # other imports go here
 
+#config for log file: Needs file name 
+#LOG_FILENAME = ''
+#logging.basicConfig(filename=LOG_FILENAME, level=logging.debug)
+
+def download_data(url):
+    '''pulls down web log file and stores it as csv'''
+    file = requests.get(url)
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
 
 def main(url):
     print(f"Running main with URL = {url}...")
