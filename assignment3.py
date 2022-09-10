bf667cb0d514b207bc26ade7d8296ce306f3313c
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-09-09 21:34:00 -0400

    Assingment3 minus extra credit

--- assignment3.py
+++ assignment3.py
@@ -1,15 +1,10 @@
 import argparse
 import requests
 import csv
-import logging
 import re
 import io
 import datetime
 # other imports go here
-
-#config for log file: Needs file name 
-#LOG_FILENAME = ''
-#logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
 def download_data(url):
     '''pulls down web log file as a string'''
@@ -18,41 +13,43 @@ def download_data(url):
 
 def csv_process(file):
     image_hits = 0
+
+    #dict to store browser hits
     browser_dict = {
         'IE': 0,
         'Safari': 0,
         'Chrome': 0,
         'Firefox': 0
     }
+    #dict to store hourly hits
+    hour_dict = {}
+   
     csv_reader = csv.reader(io.StringIO(file))
     for i, row in enumerate(csv_reader):
         path_to_file = row[0]
         datetime_accessed = row[1] # parse this using dateime.datetime.strptim()
-        try:
-            datetime_accessed = datetime.datetime.strptime(row[1], '%Y/%m/%d %H:%M:%S')
-        except ValueError:
-            pass
         browser = row[2]
         status = row[3]
         request_size = row[4]
         #print(row)
 
-        if re.search("gif|jpe?g|png$", path_to_file.lower()):
+        if re.search(r"gif|jpe?g|png", path_to_file.lower()): #searches for image file extensions at the end of a string
             image_hits += 1
         #check browser and update dict
-        elif re.search(r"\bMSIE\b", browser):
+        if re.search(r"\bMSIE\b", browser.upper()):
             browser_dict['IE'] += 1
-        elif re.search(r"\bFirefox\b", browser):
+        elif re.search(r"\bfirefox\b", browser.lower()):
             browser_dict['Firefox'] += 1
-        elif re.search(r'\bSafari\b', browser) and re.search(r'\bChrome\b', browser):
+        elif re.search(r'\bsafari\b', browser.lower()) and re.search(r'\bchrome\b', browser.lower()):
             browser_dict['Chrome'] += 1
         else:
             browser_dict["Safari"] += 1
-    
+
     avg_hits = image_hits/(i + 1) * 100
     print(f'Image requests account for {avg_hits}% of all requests.')
     popular = max(browser_dict, key=browser_dict.get)
-    print(f'The most popular browser is {popular}.')
+    print(f'The most popular browser is {popular} with {browser_dict["Chrome"]} hits.')
+    
    
 
 def main(url):
